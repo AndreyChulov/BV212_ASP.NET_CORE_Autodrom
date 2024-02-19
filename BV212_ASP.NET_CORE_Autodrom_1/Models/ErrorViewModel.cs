358ca8fa3d8212118524210f78e21003b42ba5b2
AndreyChulov <andreyc253@gmail.com>
2024-02-19 21:25:53 +0300

    First commit

---
+++ BV212_ASP.NET_CORE_Autodrom_1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BV212_ASP.NET_CORE_Autodrom_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
